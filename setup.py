3f4de6dc3e9ca8157c6f8e5f9f75c238f17dd5cc
zeke-john <67245013+zeke-john@users.noreply.github.com>
2020-06-27 17:00:38 -0700

    less space

--- setup.py
+++ setup.py
@@ -2,5 +2,4 @@ import cx_Freeze
 
 executables = [cx_Freeze.Executable("main.py")]
 
-cx_Freeze.setup( name= "Space Game", options = {"build_exe": {"packages": ["turtle", "winsound", "time"] ,"include_files":["res/images/wp3284832.gif","res/images/3nd_enemy.gif", "res/images/player.gif", "res/images/enemy.gif", "res/images/boom.gif", "res/images/2nd_enemy.gif", "res/sounds/cartoon016.wav", "res/sounds/cartoon007.wav", "res/sounds/battle003.wav"]}}, executables = executables)
-
+cx_Freeze.setup( name="Space Game", options={"build_exe": {"packages": ["turtle", "winsound", "time"],"include_files":["res/images/wp3284832.gif","res/images/3nd_enemy.gif", "res/images/player.gif", "res/images/enemy.gif", "res/images/boom.gif", "res/images/2nd_enemy.gif", "res/sounds/cartoon016.wav", "res/sounds/cartoon007.wav", "res/sounds/battle003.wav"]}}, executables = executables)
